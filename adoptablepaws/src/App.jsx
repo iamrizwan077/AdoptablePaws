8b9e1a245561bba121aebcca4a158dabbc8e7313
Rizwan Riaz <iamrizwan077@gmail.com>
2024-03-23 18:36:02 +0500

    Replaced useEffect with useQuery

--- adoptablepaws/src/App.jsx
+++ adoptablepaws/src/App.jsx
@@ -1,15 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SearchParams from "./Components/SearchParams";
 import Details from "./Components/Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 30, // From React Query version 5, gcTime is used instead of cacheTime
+      },
+    }
+});
+
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <h1>Adopt Me!</h1>
       <Routes>
         <Route path="/" element={<SearchParams />} />
         <Route path="/details/:id" element={<Details />} />
       </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- adoptablepaws/src/Components/Details.jsx
+++ adoptablepaws/src/Components/Details.jsx
@@ -1,9 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "../scripts/fetchPet";
 
 const Details = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  //From React Query v5, only Object type is supported in useQuery params
+  // earlier it was useQuery(["details", id], fetchPet);
+  const results = useQuery({
+    queryKey: ["details", id],
+    queryFn: fetchPet,
+  });
+  if (results.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  console.log(results.data.pets);
+  const pet = results.data.pets[0];
 
-  return <div>Details page for {id}</div>;
+  return (
+    <div>
+      <div>
+        {pet.name} - {pet.animal} - {pet.description}
+      </div>
+      <button>Adopt {pet.name}</button>
+    </div>
+  );
 };
 
 export default Details;
