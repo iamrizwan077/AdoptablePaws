5d0de19857d38f4a6a175d9b62caafd46b08791d
Rizwan Riaz <iamrizwan077@gmail.com>
2024-03-25 16:52:12 +0500

    Implemented modal using portals

--- adoptablepaws/src/Components/Details.jsx
+++ adoptablepaws/src/Components/Details.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "../scripts/fetchPet";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
+
   //From React Query v5, only Object type is supported in useQuery params
   // earlier it was useQuery(["details", id], fetchPet);
   const results = useQuery({
@@ -14,16 +18,22 @@ const Details = () => {
   if (results.isLoading) {
     return <h1>Loading...</h1>;
   }
-  
+
   const pet = results.data.pets[0];
 
   return (
-    
     <div>
       <div>
         {pet.name} - {pet.animal} - {pet.description}
       </div>
-      <button>Adopt {pet.name}</button>
+      <button onClick={(e) => setShowModal(true)}>Adopt {pet.name}</button>
+      {showModal ? (
+        <div>
+          <h1>Would you like to adopt {pet.name}</h1>
+          <button>Yes</button>
+          <button onClick={(e) => setShowModal(false)}>No</button>
+        </div>
+      ) : null}
     </div>
   );
 };
@@ -34,6 +44,6 @@ const DetailsWithErrorBoundary = () => {
       <Details />
     </ErrorBoundary>
   );
-}
+};
 
 export default DetailsWithErrorBoundary;
