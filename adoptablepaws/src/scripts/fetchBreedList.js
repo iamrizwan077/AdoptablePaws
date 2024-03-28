f3c7c5ec0e1886859cfcb282074668bc393aa4be
Rizwan Riaz <iamrizwan077@gmail.com>
2024-03-28 19:49:30 +0500

    Changed URL protocol to HTTPS for deployment

--- adoptablepaws/src/scripts/fetchBreedList.js
+++ adoptablepaws/src/scripts/fetchBreedList.js
@@ -1,7 +1,7 @@
 const fetchBreedList = async ({queryKey}) => {
     const animal = queryKey[1];
 
-    const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+    const res = await fetch(`https://pets-v2.dev-apis.com/breeds?animal=${animal}`);
     if (!res.ok) {
         throw new Error(`An error occurred while fetching the breed of animal ${animal}.`);
     }
--- adoptablepaws/src/scripts/fetchPet.js
+++ adoptablepaws/src/scripts/fetchPet.js
@@ -1,7 +1,7 @@
 const fetchPet = async ({queryKey}) => {
     const id = queryKey[1];
 
-    const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    const res = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
     if (!res.ok) {
         throw new Error(`An error occurred while fetching the pet id ${id}.`);
     }
